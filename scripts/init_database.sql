9572018444de8622190b20b1fb69497773dff91c
blackishgray <81298193+blackishgray@users.noreply.github.com>
2025-03-08 14:38:56 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+use master;
+
+IF EXISTS (SELECT 1 FROM sys.databases where name='DataWareHouse')
+BEGIN
+	ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWareHouse;
+END;
+GO
+-- Create the DataWareHouse Database 
+CREATE DATABASE DataWareHouse;
+GO;
+
+use DataWareHouse;
+GO;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+
 
