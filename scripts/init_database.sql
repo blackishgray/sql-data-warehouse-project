bc44bf97b3f128d940e572f7f32398ff1a8508c4
blackishgray <81298193+blackishgray@users.noreply.github.com>
2025-03-09 20:30:09 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,22 +1,38 @@
-use master;
-
-IF EXISTS (SELECT 1 FROM sys.databases where name='DataWareHouse')
-BEGIN
-	ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE DataWareHouse;
-END;
-GO
--- Create the DataWareHouse Database 
-CREATE DATABASE DataWareHouse;
-GO;
-
-use DataWareHouse;
-GO;
-
-CREATE SCHEMA bronze;
-GO
-CREATE SCHEMA silver;
-GO
-CREATE SCHEMA gold;
-
-
+	/*
+	----------------------
+	CREATE A SCHEMA 
+	----------------------
+	
+	Scripts purpose :
+		This script creates a new database named 'DataWareHouse' after checking if it already exists.
+		If the database exists, it is dropped and recreaded. Additionally, The script sets up thress schemas 
+		within the database : 'bronze', 'silver' and 'gold'.
+	
+	WARNING:
+		Running this script wiil drop and entire 'DataWareHouse' database if it exists.
+		All data in the database will be permanently deleted. Proceed with caution 
+		and ensure you have proper backups before running this script.
+	*/
+	
+	use master;
+	
+	IF EXISTS (SELECT 1 FROM sys.databases where name='DataWareHouse')
+	BEGIN
+		ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+		DROP DATABASE DataWareHouse;
+	END;
+	GO
+	-- Create the DataWareHouse Database 
+	CREATE DATABASE DataWareHouse;
+	GO;
+	
+	use DataWareHouse;
+	GO;
+	
+	CREATE SCHEMA bronze;
+	GO
+	CREATE SCHEMA silver;
+	GO
+	CREATE SCHEMA gold;
+	
+	
